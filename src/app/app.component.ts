c7280263fe48b8a39fdd4f291aec1540736a3a65
PrithvirajkumarK <prithviprithvi393@gmail.com>
2023-09-27 10:50:54 +0530

    css changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,24 @@
 import { Component } from '@angular/core';
-type anime={
-  id:string
-  title:string
-  poster:string
-  summary:string
-  trailer:string
-  languages:Array<string>
-  genres:Array<string>
-  like:number
-  dislike:number
-  censorRating:string
-}
+type anime = {
+  id: string;
+  title: string;
+  poster: string;
+  summary: string;
+  trailer: string;
+  languages: Array<string>;
+  genres: Array<string>;
+  like: number;
+  dislike: number;
+  censorRating: string;
+  releaseyear: number;
+};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Final_Project';
 }
 
-export{anime}
+export { anime };
